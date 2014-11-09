933efd87a61f857284f9e70719ada1e6a2d50373
Sreng <sreng@Srengs-MacBook-Pro.local>
2014-11-09 12:16:44 +0700

    add contact function

---
+++ phonebook.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global variable
+// int ID = 0;
+int OPTION;
+
+typedef struct contact Contact;
+
+struct contact {
+  char firstName[20], lastName[20], phoneNumber[20];
+};
+
+void writeIntoFile(Contact information) {
+	
+	// char id[4];
+	// sprintf(id, "%d", ID);
+
+    FILE *file;
+    char *fname;
+    fname = "phonebooks.txt";
+    file = fopen(fname, "a+");
+
+	// fputs(id, file);
+	// fputs("|", file);
+    fputs(information.firstName, file);
+	fputs("|", file);
+	fputs(information.lastName, file);
+	fputs("|", file);
+	fputs(information.phoneNumber, file);
+	fputs("|\n", file);
+
+	// ID++;
+    fclose(file);
+}
+
+void menu() {
+	
+	printf("\n*****************************************\n\n");
+	printf("*\tPossible Option\t\t\t*\n");
+	printf("*\t1. Add a new contact\t\t*\n");
+	printf("*\t2. Exit the program\t\t*\n");
+	printf("\n*****************************************\n\n");
+	printf("Choose an operation[1-2]: ");
+	scanf("%d", &OPTION);
+}
+
+void addNewContact() {
+	
+	Contact new_contact;
+	
+	printf("\nNew Contact\n");
+	printf("Input your last name: ");
+	scanf("%s", new_contact.firstName);
+	printf("Input your first name: ");
+	scanf("%s", new_contact.lastName);
+	printf("Input your phone number: ");
+	scanf("%s", new_contact.phoneNumber);
+	
+	writeIntoFile(new_contact);
+}
+
+void option(OPTION) {
+
+	switch (OPTION) {
+		case 1: addNewContact(); break;
+		case 2: printf("2\n");; break;
+		// case item: statements; break;
+		default: printf("No this option\n");; break;
+	}
+} 
+
+//Program run from here
+
+int main() {
+	
+	printf("Welcome!!!\n");
+
+	menu();
+	option(OPTION);
+
+	printf("\nGood bye!!!\n");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
